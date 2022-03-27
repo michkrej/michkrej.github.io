a8756908d7bdd414f702b8f2769ed029fe717c35
Michelle Krejci <michkrej@gmail.com>
2022-03-27 11:13:43 +0200

    Updated website

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -1,18 +1,19 @@
-import React from "react"
+import React from 'react';
 
-import "./about.scss"
+import './about.scss';
 
-import data from "../../../constants"
+import { about } from '../../../constants';
 
 const About = () => {
-  return (
-    <div id='about' className='about-container'>
-      <div className='about'>
-        <p>{data.about.p1}</p>
-        <p>{data.about.p2}</p>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div id='about' className='about-container'>
+			<div className='about'>
+				{about.map((text) => (
+					<p>{text}</p>
+				))}
+			</div>
+		</div>
+	);
+};
 
-export default About
+export default About;
--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,37 +1,34 @@
-import React from "react";
-import { OutboundLink } from "gatsby-plugin-google-gtag";
+import React from 'react';
+import { OutboundLink } from 'gatsby-plugin-google-gtag';
+import { contactInfo } from '../../../constants';
 
-import github from "../../images/github.svg";
-import linkedin from "../../images/linkedin.svg";
+import github from '../../images/github.svg';
+import linkedin from '../../images/linkedin.svg';
 
-import data from "../../../constants";
-
-import "./footer.scss"
+import './footer.scss';
 
 const Footer = () => {
-  return (
-      <footer>
-        <a href="mailto:michkrej@gmai.com">michkrej@gmail.com</a>
-        <div className="socials">
-          <OutboundLink
-            href={data.github}
-            target="__blank"
-            rel="noopener noreferrer"
-            
-          >
-            <img src={github} alt="Github logo"/>
-          </OutboundLink>
-          <OutboundLink
-            href={data.linkedin}
-            target="__blank"
-            rel="noopener noreferrer"
-
-          >
-            <img src={linkedin} alt="LinkedIn logo"/>
-          </OutboundLink>
-        </div>
-      </footer>
-  );
+	return (
+		<footer>
+			<a href={`mailto:${contactInfo.email}`}>{contactInfo.email}</a>
+			<div className='socials'>
+				<OutboundLink
+					href={contactInfo.github}
+					target='__blank'
+					rel='noopener noreferrer'
+				>
+					<img src={github} alt='Github logo' />
+				</OutboundLink>
+				<OutboundLink
+					href={contactInfo.linkedin}
+					target='__blank'
+					rel='noopener noreferrer'
+				>
+					<img src={linkedin} alt='LinkedIn logo' />
+				</OutboundLink>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;

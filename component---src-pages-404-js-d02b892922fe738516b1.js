3656cd281e5eb5d1143a966dcfad3b109c2ae04c
michkrej <michkrej@users.noreply.github.com>
2022-09-15 06:49:02 +0000

    deployed via Gatsby Publish Action 🎩 for 89cd5aee04d76f9999d54f9ed721b14be7fb11ce

---
+++ component---src-pages-404-js-d02b892922fe738516b1.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpersonal_website=self.webpackChunkpersonal_website||[]).push([[883],{429:function(e,t,l){l.r(t);var n=l(7294),a=l(7059),s=l(3771),r=l(9464),c={witdh:"100%",height:"100vh",display:"flex",alignItems:"center",flex:1};t.default=function(){return n.createElement(a.Z,null,n.createElement(s.Z,null),n.createElement("div",{style:c},n.createElement("p",{className:"up-and-down"},"This page doesn't exist")),n.createElement(r.Z,null))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-d02b892922fe738516b1.js.map
